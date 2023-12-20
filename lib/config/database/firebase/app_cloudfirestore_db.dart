5568f78b4b506691c70ba542d33847b99adf79d4
Herik Colares <dev.herik@gmail.com>
2023-12-20 05:11:55 -0300

    logic: initiated the dao logic

--- lib/config/database/firebase/app_cloudfirestore_db.dart
+++ lib/config/database/firebase/app_cloudfirestore_db.dart
@@ -3,18 +3,31 @@ import 'package:simple_finances/config/database/firebase/app_fireauth_db.dart';
 
 class CloudFirestoreDataBase {
   final String uid = FireAuth().getUid();
-  FirebaseFirestore get firestoredatabase => FirebaseFirestore.instance;
+  FirebaseFirestore get _firestoredatabase => FirebaseFirestore.instance;
 
-  Future persistDocument(Map<String, dynamic> doc, String path) async {
+  Future<void> persistDocument(
+      Map<String, dynamic> document, String path) async {
     // recevies a path and a map and persist it
+    await _firestoredatabase.collection(path).add(document);
   }
-  Future getCollection(String path) async {
+
+  Future<void> updateDocument(
+      String path, String document, Map<String, dynamic> documentUpdate) async {
+    _firestoredatabase.collection(path).doc(document).update(documentUpdate);
+  }
+
+  Future<QuerySnapshot> getCollection(String path) async {
     // recevies a path to the collection and return it
+    return await _firestoredatabase.collection(path).get();
   }
-  Future getDocument(String path) async {
+
+  Future<DocumentSnapshot> getDocument(String path, String document) async {
     // recevies a path to the document and return it
+    return await _firestoredatabase.collection(path).doc(document).get();
   }
-  Future deleteDocument(String path) async {
+
+  Future deleteDocument(String path, String document) async {
     // recevies a path to the document and delete it
+    await _firestoredatabase.collection(path).doc(document).delete();
   }
 }
--- lib/config/usecases/dao_finances.dart
+++ lib/config/usecases/dao_finances.dart
@@ -1,6 +1,31 @@
-class DaoFinnaces {
-  getBalance() {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:simple_finances/config/database/firebase/app_cloudfirestore_db.dart';
+
+class DaoFinances {
+  final _dataBase = CloudFirestoreDataBase();
+
+  Future<Map<String, dynamic>> getBalance(DateTime date) async {
+    DocumentSnapshot balanceSnapshot = await _dataBase.getDocument(
+        '/simple_finances/finances/years/${date.year.toString()}/months',
+        date.month.toString());
+    if (balanceSnapshot.exists) {
+      return balanceSnapshot as Map<String, dynamic>;
+    } else {
+      final map = {'document': 'Não encontrado.'};
+      return map;
+    }
+  }
+
+  Future updateDatabaseBalance(
+      Map<String, dynamic> newBalance, DateTime date) async {
+    await _dataBase.updateDocument(
+        '/simple_finances/finances/years/${date.year.toString()}/months',
+        date.month.toString(),
+        newBalance);
+  }
+
   updateBalance() {}
+
   getGoal() {}
   updateGoal() {}
 }
